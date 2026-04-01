677c89c34c1f289f958fd66d3b88963b30ac643a
Asyraf Azman <a.azman@live.iium.edu.my>
2026-04-01 14:34:00 +0800

    change shadcn file import directory

--- lib/components/dashboard/budget-alert-item-component.tsx
+++ lib/components/dashboard/budget-alert-item-component.tsx
@@ -1,5 +1,4 @@
-import { CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Card } from "../ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { CircleAlert } from "lucide-react";
 
 export default function BudgetAlertItemComponent() {
--- lib/components/dashboard/summary-component.tsx
+++ lib/components/dashboard/summary-component.tsx
@@ -1,5 +1,5 @@
+import { Card } from "@/components/ui/card";
 import { Plane } from "lucide-react";
-import { Card } from "../ui/card";
 
 export default function SummaryComponent() {
   return (
